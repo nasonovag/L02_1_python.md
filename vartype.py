7fd86dbae9547b4dbbb5992f748a2bbdca2272a0
Beezy <Dmitrix2019@yandex.ru>
2025-04-03 10:34:25 +0300

    Добавлен файл vartype.py

---
+++ vartype.py
@@ -0,0 +1,14 @@
+
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+sum_result = num1 + num2
+print(f"Сумма чисел: {sum_result}")
+
+
+user_input = input("Введите число: ")
+converted_number = int(user_input)
+print(f"Вы ввели число {converted_number}, его тип данных: {type(converted_number)}")
+
+
+age = int(input("Введите ваш возраст: "))
+print(f"Вам {age} лет.")
